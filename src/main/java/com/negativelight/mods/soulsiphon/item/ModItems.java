abfb1f03193af9311782689c16ef78bf1bef8763
Negative-light <negalight414@gmail.com>
2023-03-21 16:51:36 -0400

    Updated mod to 1.19.4

--- src/main/java/com/negativelight/mods/soulsiphon/item/ModItems.java
+++ src/main/java/com/negativelight/mods/soulsiphon/item/ModItems.java
@@ -4,7 +4,6 @@ import com.negativelight.mods.soulsiphon.item.custom.WeepingUrnItem;
 import com.negativelight.mods.soulsiphon.soulsiphon;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,8 +12,23 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, soulsiphon.MODID);
 
-    public static final RegistryObject<WeepingUrnItem> WEEPING_URN = ITEMS.register("weeping_urn", ()-> new WeepingUrnItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(64)));
-    public static final RegistryObject<WeepingUrnItem> WEEPING_URN_FULL = ITEMS.register("weeping_urn_full", ()-> new WeepingUrnItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(16).craftRemainder(WEEPING_URN.get())));
+    public static final RegistryObject<WeepingUrnItem> WEEPING_URN =
+            ITEMS.register("weeping_urn",
+                    ()-> new WeepingUrnItem(
+                            new Item.Properties()
+                                    //.tab(CreativeModeTab.TAB_TOOLS)
+                                    .stacksTo(64)
+                    )
+            );
+    public static final RegistryObject<WeepingUrnItem> WEEPING_URN_FULL =
+            ITEMS.register("weeping_urn_full",
+                    ()-> new WeepingUrnItem(
+                            new Item.Properties()
+                                     //.tab(CreativeModeTab.TAB_TOOLS)
+                                    .stacksTo(16)
+                                    .craftRemainder(WEEPING_URN.get())
+                    )
+            );
 
 
 
