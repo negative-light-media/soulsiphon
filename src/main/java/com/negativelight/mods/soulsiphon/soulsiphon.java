af715e26a8588df5423accf015b13e4ec59a4701
Nathaniel Hartlen <107813207+crazysmile11012@users.noreply.github.com>
2023-06-01 08:50:40 -0300

    fixed bug in structure blending in nether

--- src/main/java/com/negativelight/mods/soulsiphon/soulsiphon.java
+++ src/main/java/com/negativelight/mods/soulsiphon/soulsiphon.java
@@ -3,30 +3,14 @@ package com.negativelight.mods.soulsiphon;
 import com.mojang.logging.LogUtils;
 import com.negativelight.mods.soulsiphon.block.ModBlocks;
 import com.negativelight.mods.soulsiphon.item.ModItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -39,7 +23,8 @@ public class soulsiphon {
     public static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public soulsiphon() {
+    public soulsiphon()
+    {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -54,11 +39,11 @@ public class soulsiphon {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private  void clientSetup(final FMLClientSetupEvent event)
-    {
+
+    private void clientSetup(final FMLClientSetupEvent e){
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
+    private void setup(final FMLCommonSetupEvent event){
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
