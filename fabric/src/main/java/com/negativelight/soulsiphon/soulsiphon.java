cc8880f3d4e7896d193989fea5c34e7fc84c21ab
Negative_light <53065989+Negative-light@users.noreply.github.com>
2023-11-04 13:03:48 -0400

    Fixed Bug in fabric where SoulSiphon block didn't render properly. Fixes #30

--- fabric/src/main/java/com/negativelight/soulsiphon/soulsiphon.java
+++ fabric/src/main/java/com/negativelight/soulsiphon/soulsiphon.java
@@ -1,13 +1,18 @@
 package com.negativelight.soulsiphon;
 
 import com.negativelight.soulsiphon.block.ModBlocks;
+import com.negativelight.soulsiphon.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.item.CreativeModeTabs;
 
 public class soulsiphon implements ModInitializer {
-    
+
     @Override
     public void onInitialize() {
         
@@ -22,6 +27,10 @@ public class soulsiphon implements ModInitializer {
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(soulsiphon::addBuildingBlocks);
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(soulsiphon::addTools);
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(soulsiphon::addFunctionalBlocks);
+
+        //Custom Redner Layers
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SOUL_SIPHON.get(), RenderType.cutout());
     }
 
     private static void addBuildingBlocks(FabricItemGroupEntries entries) {
@@ -30,6 +39,14 @@ public class soulsiphon implements ModInitializer {
 
     private static void addTools(FabricItemGroupEntries entries) {
        // entries.accept(ModItems.GRADER_TOOL.get());
+        entries.accept(ModItems.WEEPING_URN.get());
+        entries.accept(ModItems.WEEPING_URN_FULL.get());
+    }
+
+    private static void addFunctionalBlocks(FabricItemGroupEntries entries) {
+        entries.accept(ModBlocks.SCULK_CAULDRON.get());
+        entries.accept(ModBlocks.SOUL_SIPHON.get());
+
     }
 
 
